f70539afbea4ba7b787757e592903c7056c69bbf
sigkl <sigvekl@icloud.com>
2023-06-09 05:16:43 +0200

    Daniel Gjeldseth

--- testprogramvare/player.cs
+++ testprogramvare/player.cs
@@ -3,3 +3,4 @@ public til zig er veldig kort
     penisen til hardil er liten
     rey j skal knulle oss i speedbump
 
+    //jeg er hjemme hos sigve kl 05:13. skal kjøre inn i en lastebil på motsat kjøre felt på engelsk aka leah lane
